e8f0f23b580c916b753fe603e5d2e88d9e40583c
PlaceForNick <adamm368.7@gmail.com>
2023-04-15 21:24:25 +0200

    rozdzielenie metody XYZ2flh na XYZ2flh, dms, Np

--- skrypt.py
+++ skrypt.py
@@ -3,26 +3,30 @@ from math import *
 
 class Transformacje():
     
-    
-    # a = 6378137.000
-    # e2 = 0.00669438002290
-    
     def __init__(self, podany_X, podany_Y, podany_Z): #self - parametr, ktory reprezentuje obiekt sam w sobie
         self.X = podany_X
         self.Y = podany_Y
         self.Z = podany_Z
         self.a = 6378137.000
         self.e2 = 0.00669438002290
-        
-        # print(a)
-    # @classmethod
+           
+    def Np(self, f): #promien krzywizny w I wertykale
+        a = self.a 
+        e2 = self.e2 
+        N = a / np.sqrt(1 - e2 * np.sin(f)**2)
+        return(N) 
     
-    # def Np(self, f): #promien krzywizny w I wertykale
-    #     a = self.a 
-    #     e2 = self.e2 
-    #     N = a / np.sqrt(1 - e2 * np.sin(f)**2)
-    #     return(N) 
-    
+    def dms(self, x): #zamiana wyswietlania sie stopni z ukladu 10 na uklad 60 
+        znak = ' '
+        if x < 0:
+            znak = '-'
+            x = abs(x)
+        x = x * 180/pi
+        d = int(x)
+        m = int((x - d) *  60)
+        s = (x - d - (m/60)) * 3600
+        return(f"{znak}{d:3d}°{m:2d}'{s:8.5f}''")
+      
     def xyz2flh(self): #HIRVONEN
         X = self.X 
         Y = self.Y
@@ -32,32 +36,21 @@ class Transformacje():
         p = np.sqrt(X**2 + Y**2)
         f = np.arctan(Z/(p * (1 - e2)))
         while True:
-            N = a / np.sqrt(1 - e2 * np.sin(f)**2)
+            N = self.Np(f)
             h = (p / np.cos(f)) - N
             fp = f
             f = np.arctan(Z / (p * (1 - e2 * N / (N + h))))
             if abs(fp - f) < (0.000001/206265):
                 break
-        N = a / np.sqrt(1 - e2 * np.sin(f)**2)
+        
+        N = self.Np(f)
         h = (p / np.cos(f)) - N
         l = np.arctan2(Y, X)
-    
-        dms = []
-        for x in (f,l):
-            znak = ' '
-            if x < 0:
-                znak = '-'
-                x = abs(x)
-            x = x * 180/pi
-            d = int(x)
-            m = int((x - d) *  60)
-            s = (x - d - (m/60)) * 3600
-            dms.append(f"{znak}{d:3d}°{m:2d}'{s:8.5f}''")
-        f_st = dms[0]
-        l_st = dms[1]
         
-        return(f_st,l_st,h)
-
+        f = self.dms(f)
+        l = self.dms(l)
+        return(f, l, h)
+    
 if __name__ == '__main__':
     test = Transformacje(100, 200, 300)
     test.xyz2flh()
