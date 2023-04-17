fa50030513a3a7eb0ccbd72e81d0aea4af0b0078
PlaceForNick <adamm368.7@gmail.com>
2023-04-17 12:42:58 +0200

    stworzenie klasy style; storzenie klasy NieprawidlowaWartosc; dodanie klauzury try/except/else do metody fl2PL2000

--- skrypt.py
+++ skrypt.py
@@ -1,6 +1,34 @@
 import numpy as np
 from math import *
+import os
 
+os.system("")
+
+class style():
+    BLACK = '\033[30m'
+    RED = '\033[31m'
+    GREEN = '\033[32m'
+    YELLOW = '\033[33m'
+    BLUE = '\033[34m'
+    MAGENTA = '\033[35m'
+    CYAN = '\033[36m'
+    WHITE = '\033[37m'
+    UNDERLINE = '\033[4m'
+    RESET = '\033[0m'
+    
+class NieprawidlowaWartosc(Exception):
+    '''Bład oznaczający podanie niepoprawnej lub/i nieobsługiwanej przez program wartości.
+    
+        liczba - błędna wartość podana przez użytkowanika
+        minimum - (opcjonalnie) - minimalna wymagana wartość
+        maksimum - (opcjonalnie) - maksymalna wymagana wartość
+        '''
+    def __init__(self, liczba, minimum = 'brak', maksimum = 'brak'):
+        Exception.__init__(self)
+        self.liczba = liczba
+        self.minimum  = minimum
+        self.maksimum = maksimum
+   
 class Transformacje:
 
     def __init__(self, podane_f, podane_l, podane_h = 'wartosc domyslna'):
@@ -15,6 +43,17 @@ class Transformacje:
         self.h = podane_h
         self.a = 6378137.000
         self.e2 = 0.00669438002290
+    
+    def __dms(self, x): #zamiana wyswietlania sie stopni z ukladu 10 na uklad 60 
+        znak = ' '
+        if x < 0:
+            znak = '-'
+            x = abs(x)
+        x = x * 180/pi
+        d = int(x)
+        m = int((x - d) *  60)
+        s = (x - d - (m/60)) * 3600
+        return(f"{znak}{d:3d}°{m:2d}'{s:8.5f}''")
 
     def __fromdms(self,X): #zmiana ze stopni w ukladzie dms na radiany oraz stopnie dziesietne 
         znak = 1
@@ -52,34 +91,42 @@ class Transformacje:
         l = self.l
         a = self.a
         e2 = self.e2
-        if l < radians(16.5) and l > radians(13.5): #ns = 5
-            l0 = radians(15)
-            ns = 5
-        elif l < radians(19.5) and l > radians(16.5): #ns = 6
-            l0 = radians(18)
-            ns = 6
-        elif l < radians(22.5) and l > radians(19.5): #ns = 7
-            l0 = radians(21)
-            ns = 7
-        elif l < radians(25.5) and l > radians(22.5): #ns = 8
-            l0 = radians(24)
-            ns = 8
-        else:
-            raise NameError('podana wartość l znajduje się poza zakresem stref odwzorowawcych układu współrzędnych PL2000')
-                   
-        b2 = a**2*(1 - e2)
-        ep2 = (a**2 - b2)/b2
-        dl = l - l0
-        t = tan(f)
-        n2 = ep2 * cos(f)**2
-        N = self.__Np(f)
-        sigm = self.__sigma(f)
-        xgk = sigm + (dl**2/2) * N * sin(f)*cos(f)*(1 + (dl**2/12)*cos(f)**2*(5-t**2+9*n2+4*n2**2)+ ((dl**4)/360)*cos(f)**4*(61 - 58*t**2 + t**4 + 270*n2 - 330*n2*t**2))
-        ygk = dl*N*cos(f)*(1+(dl**2/6)*cos(f)**2*(1 - t**2 + n2) + (dl**4/120)*cos(f)**4*(5 - 18*t**2 + t**4 + 14*n2 - 58*n2*t**2))
-        x2000 = xgk * m0
-        y2000 = ygk * m0 + ns * 1000000 + 500000
-        return(x2000,y2000) #,xgk,ygk)
+        try:
+            if l < radians(16.5) and l > radians(13.5): #ns = 5
+                l0 = radians(15)
+                ns = 5
+            elif l < radians(19.5) and l > radians(16.5): #ns = 6
+                l0 = radians(18)
+                ns = 6
+            elif l < radians(22.5) and l > radians(19.5): #ns = 7
+                l0 = radians(21)
+                ns = 7
+            elif l < radians(25.5) and l > radians(22.5): #ns = 8
+                l0 = radians(24)
+                ns = 8
+            else:
+                raise NieprawidlowaWartosc(self.__dms(l), minimum = 13.5, maksimum = 25.5)
+        except NieprawidlowaWartosc as nw:
+            print(style.RED + 'NieprawidlowaWartosc: ' + style.RESET)
+            print(f'podana wartość l znajduje się poza zakresem stref odwzorowawcych układu współrzędnych PL2000. '
+                  f'Obsługiwany zakres to {nw.minimum}° - {nw.maksimum}° '
+                  f'Podana przez Ciebie wartość to {nw.liczba}')
+        else:         
+            b2 = a**2*(1 - e2)
+            ep2 = (a**2 - b2)/b2
+            dl = l - l0
+            t = tan(f)
+            n2 = ep2 * cos(f)**2
+            N = self.__Np(f)
+            sigm = self.__sigma(f)
+            xgk = sigm + (dl**2/2) * N * sin(f)*cos(f)*(1 + (dl**2/12)*cos(f)**2*(5-t**2+9*n2+4*n2**2)+ ((dl**4)/360)*cos(f)**4*(61 - 58*t**2 + t**4 + 270*n2 - 330*n2*t**2))
+            ygk = dl*N*cos(f)*(1+(dl**2/6)*cos(f)**2*(1 - t**2 + n2) + (dl**4/120)*cos(f)**4*(5 - 18*t**2 + t**4 + 14*n2 - 58*n2*t**2))
+            x2000 = xgk * m0
+            y2000 = ygk * m0 + ns * 1000000 + 500000
+            return(x2000,y2000) #,xgk,ygk)
 
 if __name__=='__main__':
-    proba = Transformacje('-52 34 28.9', 40, 130)
-    print(proba.fl2PL2000())
+    proba1 = Transformacje('-52 34 28.9', 15, 130)
+    proba2 = Transformacje('-52 34 28.9', 40, 130)
+    print(proba1.fl2PL2000())
+    print(proba2.fl2PL2000())
